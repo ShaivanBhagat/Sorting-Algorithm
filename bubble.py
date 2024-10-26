76b98895a0a3fb625047293e501c351c3251b17f
ShaivanBhagat <srb140202@gmail.com>
2024-10-26 19:07:31 +0200

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,13 +1,1 @@
-# code for the bubble sorting algorithm obtained from Github https://github.com/HebaElwazzan/Sorting-Algorithms-Runtime/blob/master/bubble.py
 
-def bubble_sort(arr):
-    
-    n = len(arr)
-    for i in range(n - 1):
-        flag = 0
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                flag = 1
-        if not flag:
-            break
